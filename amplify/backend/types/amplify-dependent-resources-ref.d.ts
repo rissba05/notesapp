dce55b6ac30074aa4a5759499d78888fd115154f
rissba05 <rissba05@gmail.com>
2022-03-05 11:24:53 -0600

    amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "notesapp22s": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
