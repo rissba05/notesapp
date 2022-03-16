f961d5cc94c375d07c19aab8c5bcb27c5395e8ea
rissba05 <rissba05@gmail.com>
2022-03-16 15:45:01 -0500

    ready to load notes and keep track of them with useReducer hook

--- src/App.js
+++ src/App.js
@@ -14,8 +14,73 @@ import 'antd/dist/antd.css';
 
 import { listNotes } from './graphql/queries';
 
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { 
+    name: ''
+    , description: '' 
+  }
+};
 
-function App() {
+const reducer = (state, action) => {
+
+  switch(action.type) {
+
+    case 'SET_NOTES':
+      return { 
+        ...state
+        , notes: action.notes
+        , loading: false 
+      };
+
+    case 'ERROR':
+      return { 
+        ...state
+        , loading: false
+        , error: true 
+      };
+
+    default:
+      return {
+        ...state
+      };
+  }
+};
+
+//main function
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+  const fetchNotes = async () => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      });
+
+      dispatch({ 
+        type: 'SET_NOTES'
+        , notes: notesData.data.listNotes.items 
+      });
+
+    } catch (err) {
+      console.error('error: ', err)
+      dispatch({ 
+        type: 'ERROR' 
+      });
+    }
+  };
+
+  useEffect(
+    () => {
+    fetchNotes()
+    }
+    , []
+  );
+
   return (
     <div className="App">
       <header className="App-header">
